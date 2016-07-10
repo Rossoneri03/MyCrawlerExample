600fa0e562bd1cd98f5914e730296bf85fd4ee60
Tiffany <rossoneri03@gmail.com>
2016-07-10 13:07:12 +0800

    推+id+文字

--- src/crawler/example/PttHomework.java
+++ src/crawler/example/PttHomework.java
@@ -1,6 +1,8 @@
 package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 
 /**
@@ -14,15 +16,28 @@ import com.github.abola.crawler.CrawlerPack;
 public class PttHomework {
 	
 	public static void main(String[] args) {
-		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1458547807.A.32A.html";
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1467117389.A.62D.html";
 		
 
-		System.out.println( 
-			CrawlerPack.start()
-			    .addCookie("over18", "1")
-				.getFromHtml(uri)
-				.select(".push-tag:matchesOwn(推) + .push-userid")
-				.toString()
-		);
+//		System.out.println(
+//			CrawlerPack.start()
+//			    .addCookie("over18", "1")
+//				.getFromHtml(uri)
+//				.select(".push-tag:matchesOwn(噓) + .push-userid")
+////				.select(".push-tag:matchesOwn(推) + span")
+//				.toString()
+//		);
+
+		Document jsoup =CrawlerPack.start()
+				.addCookie("over18", "1")
+				.getFromHtml(uri);
+		for(Element elm : jsoup.select("div.push")){
+			System.out.print(elm.select(".push-tag").text());
+			System.out.print(elm.select(".push-userid").text());
+			System.out.print(elm.select(".push-content").text());
+			System.out.println( );
+		}
+
+
 	}
 }
